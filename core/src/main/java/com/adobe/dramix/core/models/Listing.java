46c9e0ef85ca22534bdae82eb4aa3e20f3a9a9ae
Kanika <kanika.a.arora@pwc.com>
2022-07-05 15:19:08 +0530

    Committing the Banner and Product AEM changes

---
+++ core/src/main/java/com/adobe/dramix/core/models/Listing.java
@@ -0,0 +1,14 @@
+package com.adobe.dramix.core.models;
+
+import java.util.List;
+
+/**
+ * @author karora044
+ * This method list all the banner items
+ * @return list
+ */
+public interface Listing {
+
+	List<BannerDetail> getBannerDetails();
+
+}
