b4c9143325f089509094edbfebbcdaa8435dc9ca
NinilaKS <ninilasurendran@gmail.com>
2022-08-04 17:43:30 +0530

    Teaser Card for home page created

---
+++ core/src/main/java/com/adobe/dramix/core/models/TeaserCard.java
@@ -0,0 +1,11 @@
+package com.adobe.dramix.core.models;
+
+import java.util.List;
+
+public interface TeaserCard {
+	/**
+	 * Method to get list for iterating HTML
+	 * @return
+	 */
+	List<Integer> getList(); 
+}
---
+++ core/src/main/java/com/adobe/dramix/core/models/impl/TeaserCardImpl.java
@@ -0,0 +1,36 @@
+package com.adobe.dramix.core.models.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.adobe.dramix.core.models.TeaserCard;
+
+@Model(adaptables = Resource.class, adapters = TeaserCard.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL, resourceType = TeaserCardImpl.RESOURCE_TYPE)
+public class TeaserCardImpl implements TeaserCard {
+
+	protected final static String RESOURCE_TYPE = "dramix/components/teasercards";
+	private List<Integer> list = new ArrayList<>();
+
+	@ValueMapValue
+	private int numberOfTeasers;
+
+	@PostConstruct
+	private void addList() {
+		for (int i = 0; i < numberOfTeasers; i++) {
+			list.add(i);
+		}
+	}
+
+	@Override
+	public List<Integer> getList() {
+		return list;
+	}
+
+}
