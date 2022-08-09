f0969197aea5ee17fdeb6203b56ccacbb26f1d61
Guru <gurumoorthy.chadramohan@pwc.com>
2022-08-10 00:53:34 +0530

    Megamenu Navigation changes for mobile also

--- ui.frontend/src/main/webpack/components/_navigation.js
+++ ui.frontend/src/main/webpack/components/_navigation.js
@@ -23,66 +23,64 @@ window.addEventListener("DOMContentLoaded", () => {
 
 window.addEventListener("DOMContentLoaded", () => {
 
-  const controls = document.querySelectorAll(".tab-control");
-  const tabs = document.querySelectorAll(".tab");
-  
-  // display none for 1-length tabs
-  for (let i = 1; i < tabs.length; i++) {
-    tabs[i].style.display = 'none';
-  }
-  
-  // set value forl all tabs
-  for (let i = 0; i < tabs.length; i++) {
-    tabs[i].setAttribute("value", i);
-  }
-  
-  // add event listener for all controls
-  for (let i = 0; i < controls.length; i++) {
-    controls[i].setAttribute("value", i);
-    controls[i].addEventListener("click", displayTab);
-  }
-  
-  // display tab function
-  function displayTab() {
-    for (let i = 0; i < tabs.length; i++) {
-      tabs[i].style.display = 'none';
-      controls[i].removeAttribute("id");
+  const someEle = document.getElementById("softwares");//Sample Element
+someEle.addEventListener("click", function(){
+  show();// <- There use your function with param in brackets
+}, false);
+
+  // ******* megamenu image onclick ********
+  let previd;
+  function show(idname){
+    if(!document.getElementById(idname.id).classList.contains("imgshow")){
+      document.getElementById(idname.id).classList.add("imgshow");
     }
-    tabs[this.value][0].style.display = 'block';
-    controls[this.value][0].setAttribute("id", "control-selected");
-}
-  
+    if(previd){
+      if(idname.id != previd){
+        child = document.getElementById(previd);
+        child.previousElementSibling.classList.remove('active')
+        child = document.getElementById(idname.id);
+        child.previousElementSibling.classList.add('active')
+        document.getElementById(previd).style.display = "none";
+        document.getElementById(idname.id).style.display = "block";
+      }
+    }
+    else{
+        child = document.getElementById(idname.id);
+        child.previousElementSibling.classList.add('active')
+        document.getElementById(idname.id).style.display = "block";
+    }
+    previd = idname.id;
+    document.getElementById("dropdownid").addEventListener('click', function (event) {
+            event.stopPropagation();
+    });
+  }
 // ******** search on click *********
-// const searchElement = document.getElementById('searchSection');
-// document.addEventListener('click', function(event) {
-//    searchElement.style.display = "block";
-//    searchElement.style.top = "100%";
+ //const searchElement = document.getElementById('searchSection');
+ // document.addEventListener('click', function(event) {
+ //   searchElement.style.display = "block";
+ //   searchElement.style.top = "100%";
 // });
 
 // ***** close search on click outside the element *******
-// const ignoreClickOnMeElement = document.getElementById('searchSection');
-// document.addEventListener('mouse', function(event) {
-//     const isClickInsideElement = ignoreClickOnMeElement.contains(event.target);
-//   if (!isClickInsideElement) {
-//     ignoreClickOnMeElement.style.display = 'none';
-//   }
-// });
+ const ignoreClickOnMeElement = document.getElementById('searchSection');
+ document.addEventListener('mouse', function(event) {
+     const isClickInsideElement = ignoreClickOnMeElement.contains(event.target);
+   if (!isClickInsideElement) {
+     ignoreClickOnMeElement.style.display = 'none';
+   }
+ });
 
 });
 
 
-
 window.addEventListener("DOMContentLoaded", () => {
-  document.addEventListener("mouseover", function (event) {          
-      if (!event.target.matches(".dropdown-hover")) {
-          myFunction();
-      }
-   },
-      false
-  );
- 
-  function myFunction() {
-    
+  
+  // ******* Megamenu Toolbox onhover********//
+  const targetToolbox = document.querySelector(".dropdown-hover-toolbox");
+  targetToolbox.addEventListener("mouseover", mOver, false);
+  targetToolbox.addEventListener("mouseout", mOut, false);
+
+  function mOver() {
     const bgChangeTopbar= document.getElementById('cmp-bg-change');
     const bgChangeNavbar= document.getElementById('cmp-bg-navbar');
     
@@ -95,22 +93,9 @@ window.addEventListener("DOMContentLoaded", () => {
       bgChangeNavbar.classList.add('cmp-bg-change-navbar');
       bgChangeNavbar.style.visibility='unset';
     }
+  }
 
-  }  
-
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-  document.addEventListener("mouseout", function (event) {          
-      if (!event.target.matches(".dropdown-hover")) {
-          myFunction();
-      }
-   },
-      false
-  );
- 
-  function myFunction() {
-    
+  function mOut() {  
     const someElement= document.getElementById('cmp-bg-change');
     const someElement1= document.getElementById('cmp-bg-navbar');
 
@@ -121,7 +106,44 @@ window.addEventListener("DOMContentLoaded", () => {
     if(someElement1){
       someElement1.classList.remove('cmp-bg-change-navbar');      
     }
+  }
 
-  }  
+
+  // ******* Megamenu solution onhover********//
+  const targetSolution = document.querySelector(".dropdown-hover-solution");
+  targetSolution.addEventListener("mouseover", msOver, false);
+  targetSolution.addEventListener("mouseout", msOut, false);
+
+  function msOver() {
+    const bgChangeTopbar= document.getElementById('cmp-bg-change');
+    const bgChangeNavbar= document.getElementById('cmp-bg-navbar');
+    
+    if(bgChangeTopbar){
+      bgChangeTopbar.classList.add('cmp-bg-change-top');
+      bgChangeTopbar.style.visibility='unset';
+    }
+
+    if(bgChangeNavbar){
+      bgChangeNavbar.classList.add('cmp-bg-change-navbar');
+      bgChangeNavbar.style.visibility='unset';
+    }
+  }
+
+  function msOut() {
+    const someElement= document.getElementById('cmp-bg-change');
+    const someElement1= document.getElementById('cmp-bg-navbar');
+
+    if(someElement){
+      someElement.classList.remove('cmp-bg-change-top');     
+    }
+
+    if(someElement1){
+      someElement1.classList.remove('cmp-bg-change-navbar');      
+    }
+  }
+
+
+
 
 });
+
